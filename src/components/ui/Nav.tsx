bb73705db45a4fb1d743d18eab21aa467576d1f3
Prashanth Chowdary <prashanth.chowdary@webileapps.com>
2026-02-06 17:28:19 +0530

    moved footer and nav  to its own components

--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -1,10 +1,14 @@
+import Divider from "./Divider";
+
+
 export default function Nav() {
-  return (
-    <div className=" mx-8 pt-8 h-16 flex items-center">
-        <div className=" text-2xl font-bold">
-        <p>Code</p>
-        <p>Contributor</p>
-        </div>
+  return(
+    <>
+    <div className="flex flex-col gap-3 h-[120px] justify-center leading-5">
+      <h1 className="text-5xl">OpenSource Compass</h1>
+      <p className="text-[12px] font-mono  tracking-widest">INTELLIGENT PROJECT RECOMMENDATION SYSTEM</p>
     </div>
+    <Divider className="my-10"/>
+    </>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,17 +2,14 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Divider from "@/components/ui/Divider";
 import  Github  from "../icons/github.svg";
-import Title from "@/components/ui/Title";
+import Nav from "@/components/ui/Nav";
+import Footer from "@/components/ui/Footer";
 
 export default function Login() {
   return (
-    <div className="mt-10 lg:px-[140px] md:px-[100px] sm:px-[50px] px-[20px]">
-      <div className="flex flex-col gap-3 h-[120px] justify-center leading-5">
-        <h1 className="text-5xl">OpenSource Compass</h1>
-        <p className="text-[12px] font-mono  tracking-widest">INTELLIGENT PROJECT RECOMMENDATION SYSTEM</p>
-      </div>
-      <Divider className="my-10"/>
-      <div className="min-h-[calc(100vh-400px)] flex items-center">
+    <div className="mt-5 lg:px-[140px] md:px-[100px] sm:px-[50px] px-[20px]">
+      <Nav/>
+      <div className="min-h-[calc(100vh-350px)] flex items-center">
       <Card className="lg:w-1/2 py-10 w-full mx-auto text-center">
         <CardHeader>
           <CardTitle className="text-2xl">Sign Up with GitHub</CardTitle>
@@ -23,7 +20,9 @@ export default function Login() {
             <img src={Github} alt="" className="w-5 h-5" />
             Continue with GitHub
           </Button>
-          <CardDescription className="text-md my-15 lg:w-[70%] mx-auto font-inter">Connect with your github account and analyze your contribution history, skills, and interests we use this data to find prefect open source project for you.</CardDescription>
+          <CardDescription className="text-md my-15 lg:w-[70%] mx-auto font-inter">
+            Connect with your github account and analyze your contribution history, skills, and interests we use this data to find prefect open source project for you.
+          </CardDescription>
         </CardContent>
         <Divider className="w-[calc(100%-40px)] mx-auto"/>
         <CardFooter className="flex justify-center">
@@ -31,9 +30,7 @@ export default function Login() {
         </CardFooter>
       </Card>
       </div>
-      <Divider className="my-10"/>
-      <p className="text-center font-mono text-sm my-10">© 2026 OpenSource Compass. All rights reserved.</p>
-      <Title title="OpenSource Compass"/>
+      <Footer/>
     </div>
   )
 }
