6b306cc2abb615ec563a5bd46b011d8f1b7fcd0b
Prashanth Chowdary <prashanth.chowdary@webileapps.com>
2026-02-06 17:28:28 +0530

    footer

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,10 @@
+import Divider from './Divider'
+
+export default function Footer() {
+  return (
+    <div>
+        <Divider className="my-10"/>
+      <p className="text-center font-mono text-sm my-10">© 2026 OpenSource Compass. All rights reserved.</p>
+    </div>
+  )
+}
