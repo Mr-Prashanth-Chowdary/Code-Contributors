2ac557706e07294b84fd256359e26abb5a5a2ff3
Prashanth Chowdary <prashanth.chowdary@webileapps.com>
2026-02-09 10:53:38 +0530

    feat: Add Projectpage, configure GitHub OAuth environment variables, and make Divider component's className prop optional.

--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -1,6 +1,6 @@
 
-export default function Divider({className}: {className: string}) {
-    const width = className.match(/w-\[(.+?)\]/)?.[1];
+export default function Divider({className}: {className?: string}) {
+    const width = className?.match(/w-\[(.+?)\]/)?.[1];
     return (
         <div className={`h-0.5 w-[${width}] bg-black/10 ${className}`}></div>
     )
